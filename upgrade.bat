350ab6028fa0cb1468793630d137449e3f63a354
drapsema <sema@drap.hr>
2019-08-12 17:18:59 +0200

    Upgrade NOW possible

--- upgrade.bat
+++ upgrade.bat
@@ -25,10 +25,36 @@ if '%errorlevel%' NEQ '0' (
 ::--------------------------------------
 
 rem Create uninstall folder
+@RD /S /Q "%uplocation%"
 mkdir %temp%\DrapToolUpgrade
 set uplocation=%temp%\DrapToolUpgrade
 echo Upgrade Location created
 
+C:\tools\wget.exe -P "%uplocation%" https://raw.githubusercontent.com/mariosemes/DrapTool/master/DrapTool/version.txt
+TIMEOUT 5
+findstr /vig:C:\DrapTool\version.txt %uplocation%\version.txt > %uplocation%\upgrade.txt
+
+cls
+echo Your Version:
+type C:\DrapTool\version.txt
+echo.
+echo Current Version
+type "%uplocation%\version.txt"
+echo.
+
+set fileIsBlank=0
+for /f %%i in ("%uplocation%\upgrade.txt") do set fileIsBlank=%%~zi
+if %fileIsBlank% gtr 0 (
+	echo The app needs an update...
+	TIMEOUT 5
+	goto :upgrade
+) else (
+	echo Your app is up-to-date.
+	call :deletefiles
+	exit /b
+)
+
+:upgrade
 rem Download new master from GIT
 C:\tools\wget.exe -P "%uplocation%" https://github.com/mariosemes/DrapTool/archive/master.zip 
 
@@ -40,5 +66,11 @@ xcopy /s C:\DrapTool\installation\uninstall.bat %uplocation%
 
 call %uplocation%\uninstall.bat
 call %uplocation%\DrapTool-master\install.bat
+call :deletefiles
+exit /b
 
+:deletefiles
 @RD /S /Q "%uplocation%"
+echo Everything is done.
+TIMEOUT 5
+exit /b
