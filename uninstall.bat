fcba2613c6a7e4327ae2e624f3dfb149a045a72b
drapsema <sema@drap.hr>
2019-08-12 09:12:49 +0200

    Uninstall made easier

--- uninstall.bat
+++ uninstall.bat
@@ -24,8 +24,101 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0"
 ::--------------------------------------
 
+
+call :pngremover
+cls
+echo DrapTool Optimize PNG uninstalled
+call :jpegremover
+cls
+echo DrapTool Optimize PNG uninstalled
+echo DrapTool Optimize JPG uninstalled
+call :mp4remover
+cls
+echo DrapTool Optimize PNG uninstalled
+echo DrapTool Optimize JPG uninstalled
+echo DrapTool Edit MP4 uninstalled
+call :mp4remover2
+cls
+echo DrapTool Optimize PNG uninstalled
+echo DrapTool Optimize JPG uninstalled
+echo DrapTool Edit MP4 uninstalled
+echo DrapTool ANYtoMP4 uninstalled
+call :faviconremover
+cls
+echo DrapTool Optimize PNG uninstalled
+echo DrapTool Optimize JPG uninstalled
+echo DrapTool Edit MP4 uninstalled
+echo DrapTool ANYtoMP4 uninstalled
+echo DrapTool Favicon uninstalled
+call :renderremover
+cls
+call :deletefolder
+cls
+echo DrapTool Optimize PNG uninstalled
+echo DrapTool Optimize JPG uninstalled
+echo DrapTool Edit MP4 uninstalled
+echo DrapTool ANYtoMP4 uninstalled
+echo DrapTool Favicon uninstalled
+echo DrapTool Renderer uninstalled
+timeout /T 5
+goto :eof
+
+:pngremover
+reg delete "HKEY_CLASSES_ROOT\*\shell\DrapTool Optimize PNG" /f
+reg delete "HKEY_CLASSES_ROOT\Directory\Background\shell\DrapTool Optimize all PNGs" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\pngbest" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png10" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png20" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png30" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png40" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png50" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png60" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png70" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png80" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png90" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\png100" /f
+goto :eof
+
+:jpegremover
+reg delete "HKEY_CLASSES_ROOT\*\shell\DrapTool Optimize JPG" /f
+reg delete "HKEY_CLASSES_ROOT\Directory\Background\shell\DrapTool Optimize all JPGs" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg10" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg20" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg30" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg40" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg50" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg60" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg70" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg80" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg90" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\jpg100" /f
+goto :eof
+
+:mp4remover
+reg delete "HKEY_CLASSES_ROOT\*\shell\DrapTool Edit MP4" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\optimp4" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\mp4towebm" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\mp4toogg" /f
+goto :eof
+
+:mp4remover2
+reg delete "HKEY_CLASSES_ROOT\*\shell\DrapTool ANYtoMP4" /f
+goto :eof
+
+:faviconremover
+reg delete "HKEY_CLASSES_ROOT\*\shell\DrapTool Favicon" /f
+goto :eof
+
+:renderremover
+reg delete "HKEY_CLASSES_ROOT\*\shell\DrapTool Render" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\lossless" /f
+reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\CommandStore\shell\converting" /f
+goto :eof
+
+:deletefolder
 call DrapTool\installation\uninstall.bat
 @RD /S /Q "C:\DrapTool"
+goto :eof
 
 
 
