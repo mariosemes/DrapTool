94ee25a40f4bcd0e04535c02ce4a6265d0a0f390
Mario Semeš <mario.semes@gmail.com>
2023-10-09 10:49:04 +0200

    Some fixes

--- DrapTool/DrapToolimageOptimize.bat
+++ DrapTool/DrapToolimageOptimize.bat
@@ -10,19 +10,30 @@ rem ---------------------------------
 rem setting variables from parameters
 rem ---------------------------------
 cls
-set filelocation=%1
-set filelocation=%filelocation:"=%
-set quality=%2
-set quality=%quality:"=%
-set filetype=%3
-set filetype=%filetype:"=%
+set "filelocation=%~1"
+set "quality=%~2"
+set "filetype=%~3"
+
+rem Parameter validation
+if "%filelocation%"=="" (
+    echo Input file or folder is missing.
+    goto :eof
+)
+if "%quality%"=="" (
+    echo Quality setting is missing.
+    goto :eof
+)
+if "%filetype%"=="" (
+    echo File type is missing.
+    goto :eof
+)
 
 rem --------------------------------------------------------
 rem checking what file type it is and redirecting to section
 rem --------------------------------------------------------
 if %filetype%==png (
 	set app=%scriptpath%\library\pngquant.exe
-	set settings=--nofs --speed 1 --quality %quality% --strip
+	set settings=--nofs --force --speed 1 --quality %quality% --strip
 	set outputsett=--output
 	goto :optimization
 ) else if %filetype%==jpg (
@@ -48,8 +59,11 @@ rem -----------------------
 		call %scriptpath%\PathExtractor.bat filename "%filelocation%" file_name
 		call %scriptpath%\PathExtractor.bat filewithoutextension "%filelocation%" file_ext
 
-		%app% %settings% "%filepath%\%filename%" %outputsett% "%filepath%\%filewithoutextension%-%quality%.%filetype%"
-		echo %filepath%\%filewithoutextension%-%quality%.%filetype% generated
+		CALL :foldercheck
+		copy "%filepath%\%filename%" "%filepath%\Originals\%filename%"
+
+		%app% %settings% "%filepath%\%filename%" %outputsett% "%filepath%\%filename%"
+		echo "%filepath%\%filename%" generated
 		goto :eof
 
 	:batch
@@ -65,16 +79,19 @@ rem -----------------------
 		call %scriptpath%\PathExtractor.bat filename %name% file_name
 		call %scriptpath%\PathExtractor.bat filewithoutextension %name% file_ext
 
-		%app% %settings% %name% %outputsett% "%filelocation%\optimized %filetype% q%quality%\%filename%"
-		echo optimized %filetype% q%quality%\%filename% generated
+		CALL :foldercheck
+		copy "%filepath%\%filename%" "%filepath%\Originals\%filename%"
+
+		%app% %settings% "%filepath%\%filename%" %outputsett% "%filepath%\%filename%"
+		echo "%filepath%\%filename%" generated
 		goto :eof
 
 rem ------------------------------------------
 rem Check if the folder exists, if not, create
 rem ------------------------------------------
 :foldercheck
-	if not exist "%filelocation%\optimized %filetype% q%quality%" (
-		mkdir "%filelocation%\optimized %filetype% q%quality%"
+	if not exist "%filepath%\Originals" (
+		mkdir "%filepath%\Originals"
 	) else (
-		echo folder optimized-%quality% exists
+		echo folder Originals exists
 	)
